7e59bc3472aded0c37b1b5a70b07bf03dc3f2516
SStuckey88 <sstuc2@uic.edu>
2025-11-05 14:30:33 -0600

    making server code cleaner still needs work for actual functionality though

--- server/server.ino
+++ server/server.ino
@@ -73,40 +73,24 @@ void loop() {
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the HTTP request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard HTTP response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
+        Message c = client.read();
+    
+    
+    
+        //decide what to do here please
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
     }
     // give the web browser time to receive the data
     unsigned long waiter = millis();
@@ -118,6 +102,17 @@ void loop() {
     client.stop();
     Serial.println("client disconnected");
   }
+
+  //read data (client has example)
+
+  //send to pi
+
+
+
+
+
+
+
 }
 
 
