4e5309701dcaf0fe82830cbf7aee9576e4aae59c
SStuckey88 <sstuc2@uic.edu>
2025-11-21 17:54:54 -0600

    changed some things

--- server/server.ino
+++ server/server.ino
@@ -11,15 +11,43 @@
   https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#wi-fi-web-server
  */
 
-#include "WiFiS3.h"
-#include "Message.h"
+#include <WiFiS3.h>
+#include <DHT11.h>
+#include <AGS02MA.h>
+#include <TimeLib.h>
+#include <Wire.h>
+#include <SparkFunBME280.h>
+
+BME280 pressureSensor;
+DHT11 dht11(2);
+AGS02MA AGS(26);
+
+int JoyXPin = A0;
+int JoyYPin = A1;
+
+char serialSend[100];
+
+int temp = 0;
+int hum = 0;
+int tvoc = 0;
+float barPres = 0.0;
+
+unsigned long previousMillis = 0;
+unsigned long previousJoystickMillis = 0;
+long interval = 100;
+
+int xRead = 0;
+int yRead = 0;
+int xVal = 0;
+int yVal = 0;
+int xPrev = 0;
+int yPrev = 0;
 
 
 
-#include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "GovernmentSecurity";        // your network SSID (name)
+char pass[] = "5MfcrCw6";    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;                 // your network key index number (needed only for WEP)
 
 IPAddress staticIP(192, 168, 1, 100);
@@ -30,9 +58,34 @@ int status = WL_IDLE_STATUS;
 
 WiFiServer server(80);
 
+void waiter() {
+  unsigned long waiter = millis();
+    while (millis() - waiter < 100) {
+        int x = 5;
+    }
+}
+
+void sendData() { 
+  return;
+
+}
+
 void setup() {
-  //Initialize serial and wait for port to open:
+
   Serial.begin(9600);
+
+  Wire.begin();
+
+  dht11.setDelay(10);
+    
+  pressureSensor.setI2CAddress(0x76);
+  if(pressureSensor.beginI2C() == false) Serial.println("EBarometric Pressure sensor connection failed");
+
+  setSyncProvider(requestTimeSync); 
+
+
+
+  //Initialize serial and wait for port to open:
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -49,7 +102,7 @@ void setup() {
     Serial.println("Please upgrade the firmware");
   }
 
-  WiFi.config(staticIp);
+  WiFi.config(staticIP);
 
   // attempt to connect to WiFi network:
   while (status != WL_CONNECTED) {
@@ -57,38 +110,85 @@ void setup() {
     Serial.println(ssid);
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
-
+    Serial.println(status);
     // wait 10 seconds for connection:
-    unsigned long waiter = millis();
-    while (millis() - waiter < 10000) {
-        int x = 5
-    }
+    waiter();
   }
   server.begin();
+  Serial.println(WiFi.localIP());
   // you're connected now, so print out the status:
   printWifiStatus();
 }
 
-Message Parser(string messages) {
 
-}
 
-void waiter() {
-  unsigned long waiter = millis();
-    while (millis() - waiter < 10) {
-        int x = 5
-    }
-}
 
-void sendData(String) { 
-
-}
 
 void gatherData() {
 
+  unsigned long currentMillis = millis();
+  //Read Joystick data every 100ms - send to Pi if changed since last read
+    if (currentMillis - previousJoystickMillis >= interval) {
+        xRead = analogRead(JoyXPin);
+        yRead = analogRead(JoyYPin);
+        xVal = map(xRead, 0, 1023, -1, 1);
+        yVal = map(yRead, 0, 1023, 1, -1);
+        if(xVal != xPrev || yVal != yPrev) {
+            sprintf(serialSend, "Jx=%d;y=%d", xVal, yVal);
+            Serial.println(serialSend);
+            xPrev = xVal;
+            yPrev = yVal;
+        }
+        previousJoystickMillis = currentMillis;
+    }
+
+    if (currentMillis - previousMillis >= (interval*50)) {
+        if(timeStatus() == timeSet) {
+            time_t time = now();
+            unsigned long seconds = (unsigned long) time;
+            int result = dht11.readTemperatureHumidity(temp, hum);
+            barPres = pressureSensor.readFloatPressure();
+            tvoc = AGS.readPPB();
+
+            if (result == 0) {
+                sprintf(serialSend, "Dtime=%lu;temp=%d;hum=%d;pres=%.0f;tvoc=%d", seconds, temp, hum, barPres, tvoc);
+                Serial.println(serialSend);
+            } else {
+                // Print error message based on the error code.
+                Serial.println("ETemp/Humidity sensor connection failed");
+            }
+        } else {
+            requestTimeSync();
+        }
+        previousMillis = currentMillis;
+    }
+
+    if (Serial.available()) {
+        processSyncMessage();
+    } 
 
 
- sendData(String)
+ sendData();
+}
+
+time_t requestTimeSync()
+{
+  Serial.println("T");  
+  return 0; // the time will be sent later in response to serial mesg
+}
+
+void processSyncMessage() {
+  unsigned long pctime;
+
+  if(Serial.find('T')) {
+    //setTime(int hr,int min,int sec,int day, int month, int yr);
+     pctime = Serial.parseInt();
+    setTime(pctime); // Sync Arduino clock to the time received on the serial port
+  }
+}
+
+void Parser() {
+  sendData();
 }
 
 
@@ -99,12 +199,12 @@ void loop() {
   if (client) {
     Serial.println("new client");
     while (client.connected()) {
-      if (client.available()) {
-        String c = client.read();
+      while (client.available()) {
+        char c = client.read();
+        Parser();
         waiter();
-        sendData(c);
       }
-      gatherdata();
+      gatherData();
     }
     // give the web browser time to receive the data
 
